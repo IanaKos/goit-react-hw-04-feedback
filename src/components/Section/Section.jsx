58fadf327f34492a647d7e11b29c0c200c09a234
IanaKos <95440446+IanaKos@users.noreply.github.com>
2024-01-20 19:28:10 +0100

    added react hooks

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import css from './section.module.css';
+import PropTypes from 'prop-types';
+
+const Section = ({ title, children }) => (
+  <section>
+    <h2 className={css.title}>{title}</h2>
+    {children}
+  </section>
+);
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
+export default Section;
